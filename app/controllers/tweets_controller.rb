32415122410beeea9cf12922c11d3bb73dfe3166
Bryce Kerley <bkerley@brycekerley.net>
2014-06-05 22:46:31 -0400

    tweets index stubs

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,9 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.limit(100).order(created_at: :desc, id_number: :desc)
   end
 
   def show
+    @tweet = Tweet.find_by id_number: params[:id]
   end
 end
