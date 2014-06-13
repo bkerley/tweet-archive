c03615be43a6993273fa4071235964011ecccad0
Bryce Kerley <bkerley@brycekerley.net>
2014-06-13 11:44:53 -0400

    mapquest support

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -5,4 +5,11 @@ module TweetsHelper
 
     "https://maps.googleapis.com/maps/api/staticmap?key=#{key}&center=#{coord}&size=128x128&zoom=13&markers=size:tiny|#{coord}"
   end
+
+  def mq_url(t)
+    coord = t.latlong.join(',')
+    key = ENV['MAPQUEST_KEY']
+
+    "http://open.mapquestapi.com/staticmap/v4/getmap?key=#{key}&size=128,128&zoom=13&center=#{coord}&pois=mcenter,#{coord}"
+  end
 end
