dbdda0b35f8f1d71e4e5e7e1fbd1512a5e4b76af
Bryce Kerley <bkerley@brycekerley.net>
2014-06-13 17:08:23 -0400

    foundation-ify

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -4,12 +4,16 @@ module TweetsHelper
     key = ENV['GMAPS_KEY']
 
     "https://maps.googleapis.com/maps/api/staticmap?key=#{key}&center=#{coord}&size=128x128&zoom=13&markers=size:tiny|#{coord}"
+  rescue 
+    ''
   end
 
   def mq_url(t)
     coord = t.latlong.join(',')
     key = ENV['MAPQUEST_KEY']
 
-    "http://open.mapquestapi.com/staticmap/v4/getmap?key=#{key}&size=128,128&zoom=13&center=#{coord}&pois=mcenter,#{coord}"
+    "http://open.mapquestapi.com/staticmap/v4/getmap?key=#{key}&size=128,128&zoom=13&center=#{coord}&pois=mcenter,#{coord}"  
+  rescue 
+    ''
   end
 end
