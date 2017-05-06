01c40c51d0bd468a92af9a84c9a48af2be694ed7
Bryce Kerley <bkerley@brycekerley.net>
2017-05-05 21:10:55 -0400

    fill in gaps

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -12,4 +12,35 @@ class Timeline
       end
     end
   end
+
+  def self.fill_range(range_open, range_close)
+    count = 0
+    loop do
+      arr = TWITTER_CLIENT.user_timeline('bonzoesc',
+                                         count: 200,
+                                         since_id: range_open,
+                                         max_id: range_close - 1)
+
+      min_range = range_close + 1
+      Tweet.transaction do
+        arr.each do |api_tweet|
+          Tweet.from_api api_tweet
+
+          if api_tweet.id < min_range
+            min_range = api_tweet.id.to_i
+          end
+        end
+      end
+
+      count += 1
+
+      if arr.length < 190
+        p arr.length
+        return count
+      end
+
+      range_close = min_range
+      sleep 1
+    end
+  end
 end
