e212e19a7d9e2aff1dee02349827934795f7c1a0
Bryce Kerley <bkerley@brycekerley.net>
2017-11-06 20:33:35 -0500

    twitpic hosts images on s3 now

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,10 +1,19 @@
-Rails.application.configure do
-  config.paperclip_defaults = {
-    storage: :s3,
+opts = {
+  storage: :s3,
+  url: ':s3_alias_url',
+  s3_credentials: {
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    s3_region: 'us-east-1',
     bucket: 'bonzoesc-tweet-archive',
-    s3_credentials: {
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  },
+  s3_host_alias: 'tam.bonzoesc.net',
+  hash_secret: 'brick parker',
+  hash_data: ':class/:attachment/:id',
+  path: ':hash/:style.:extension',
+  s3_protocol: 'https'
+}
+
+opts.each do |k,v|
+  Paperclip::Attachment.default_options[k] = v
 end
